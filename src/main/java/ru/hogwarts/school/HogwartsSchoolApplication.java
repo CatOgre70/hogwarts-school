ea55489ba5c03be7a12a37787c3ee8bcd439bafc
Vasily Demin <vasily.demin@gmail.com>
2022-08-22 19:30:10 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/HogwartsSchoolApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsSchoolApplication.class, args);
+    }
+
+}

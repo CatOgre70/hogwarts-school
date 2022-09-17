531e1121fcf74739622b63257c3e39eca36615bf
Vasily Demin <vasily.demin@gmail.com>
2022-09-17 12:01:29 +0300

    Homework 4.2 Managing SQL schemas (script421.sql was modified)

--- scripts421.sql
+++ scripts421.sql
@@ -1,13 +1,6 @@
 \c homework_4.2
-create table faculty (
-                         id serial primary key,
-                         name text,
-                         color text
-);
 alter table faculty add constraint name_color_unique unique (name, color);
-create table student (
-                         id serial primary key,
-                         name text not null unique,
-                         age integer check ( age > 15 ) default 20,
-                         faculty_id serial references faculty (id)
-);
+alter table student add constraint name_not_null check (name is not null);
+alter table student add constraint name_unique unique (name);
+alter table student add constraint age_larger_than_15 check (age > 15);
+alter table student alter age set default 20;
